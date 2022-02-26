2f3dfea717625b7532107cda6d222823a491ce56
Yad <97409381+OCRenkist@users.noreply.github.com>
2022-02-25 22:00:29 -0500

    Spacing.

--- voice2json.py
+++ voice2json.py
@@ -21,20 +21,20 @@ def convert_to_duration(s):
 def serialize_general_to_record(raw):
 	soup = BeautifulSoup(raw, 'html.parser')
 	contributors = []
-	for contributor in soup.find_all('div', class_='contributor'):
+	for contributor in soup.find_all('div', class_ = 'contributor'):
 		contributors.append({
-			'name': contributor.find('span', class_='fn').string or '',
-			'tel': convert_to_tel(contributor.find('a', class_='tel')['href'])
+			'name': contributor.find('span', class_ = 'fn').string or '',
+			'tel': convert_to_tel(contributor.find('a', class_ = 'tel')['href'])
 		})
 	record = {
 		'tags': [convert_to_type(a['href']) for a in
-				soup.find_all('a', rel='tag')],
-		'date': soup.find('abbr', class_='published')['title'],
+				soup.find_all('a', rel = 'tag')],
+		'date': soup.find('abbr', class_ = 'published')['title'],
 		'contributors': contributors
 	}
-	if soup.find('abbr', class_='duration') is not None:
+	if soup.find('abbr', class_ = 'duration') is not None:
 		record['duration'] = convert_to_duration(
-			soup.find('abbr', class_='duration')['title'])
+			soup.find('abbr', class_ = 'duration')['title'])
 	return record
 # 
 def serialize_text_messages_to_record(raw):
@@ -46,24 +46,24 @@ def serialize_text_messages_to_record(raw):
 	conversation = []
 	contributors = []
 	# 
-	for contributor in soup.find_all('cite', class_='sender'):
+	for contributor in soup.find_all('cite', class_ = 'sender'):
 		# Messages from others are in the "span" tag and messages from you
 		# are in the "abbr" tag
-		if contributor.find('span', class_='fn'):
+		if contributor.find('span', class_ = 'fn'):
 			sender.append({
-				'name': contributor.find('span', class_='fn').string or '',
+				'name': contributor.find('span', class_ = 'fn').string or '',
 				'tel': convert_to_tel(
-					contributor.find('a', class_='tel')['href'])
+					contributor.find('a', class_ = 'tel')['href'])
 			})
-		if contributor.find('abbr', class_='fn'):
+		if contributor.find('abbr', class_ = 'fn'):
 			sender.append({
-				'name': contributor.find('abbr', class_='fn').string or '',
+				'name': contributor.find('abbr', class_ = 'fn').string or '',
 				'tel': convert_to_tel(
-					contributor.find('a', class_='tel')['href'])
+					contributor.find('a', class_ = 'tel')['href'])
 			})
 	for message in soup.find_all('q'):
 		messages.append(message.text)
-	for date in soup.find_all('abbr', class_='dt'):
+	for date in soup.find_all('abbr', class_ = 'dt'):
 		dates.append(date['title'])
 	for item in sender:
 		if item not in contributors:
@@ -107,8 +107,8 @@ def serialize_files_to_json(paths):
 			else:
 				serialized = serialize_general_to_record(f.read())
 				records.append(serialized)
-	records.sort(key=itemgetter('date'))
-	return json.dumps({'records': records}, indent=4)
+	records.sort(key = itemgetter('date'))
+	return json.dumps({'records': records}, indent = 4)
 # 
 def main():
 	parser = argparse.ArgumentParser()
@@ -121,8 +121,9 @@ def main():
 		type    = argparse.FileType('w'),
 		default = sys.stdout,
 		help    ='Where to write JSON output (default: stdout)')
+	args  = parser.parse_args()
 	files = glob.glob(os.path.join(args.source, '*.html'))
-	json = serialize_files_to_json(files)
+	json  = serialize_files_to_json(files)
 	with args.output as f:
 		f.write(json)
 if __name__ == '__main__':
